531b255934b62651b88528836a11530866b2b59b
pjwhite08 <pamelajeanwhite@gmail.com>
2021-07-28 09:51:01 -0500

    organized, started to set up conditional for being assigned to only one vehicle at a time

--- main.js
+++ main.js
@@ -1,78 +1,72 @@
 //The Vehicle can only GO once the correct title of crewmember is on it 
 
 //Types of vehicles
-//bus
-//train
-//plane
-//boat
+  //bus
+  //train
+  //plane
+  //boat
+
+//Titles:
+  //pilot (must be present in plane)
+  //captain (must be present in boat)
+  //engineer (must be present in train)
+  //driver(must be present in driver)
 
 class Vehicle {
-  //properties available to the vehicle:
-  //name/ identifier/ plate number or the vehicle/ unique identifier
-  //type of vehicle
-  //crewAssigned - an array of crew members (objects) assigned to the vehicle  
   
   constructor (identifier, type, crewAssigned) {
+    //name/ identifier/ plate number or the vehicle/ unique identifier
     this.identifier = identifier
+    //type of vehicle
     this.type = type
+    //crewAssigned - an array of crew members (objects) assigned to the vehicle  
     this.crewAssigned = []
   }
 
   canGo() {
     console.log(`CG this vehicle is a: ${this.type}`)
-    //for loop
+
       //for all the crewAssigned 
     for (let i=0; i < this.crewAssigned.length; i++) {
       console.log(`CG length of crew assigned: ${this.crewAssigned.length}`) 
-      let currentCrewMember = this.crewAssigned[i]
-      // console.log(`CG current crew member: ${JSON.stringify(currentCrewMember, null, 2)}`)
 
-      let currentCrewTitle = currentCrewMember.title
+      let currentCrewTitle = this.crewAssigned[i].title
       console.log(`CG current crew member's title : ${JSON.stringify(currentCrewTitle, null, 2)}`)
 
       //if/ then. if the/ any crewMember's title is the correct title for this type of vehicle
+      //then the vehicle can go
       if ((currentCrewTitle == "Pilot" && this.type == "Plane") || (currentCrewTitle == "Driver" && this.type == "Bus") || (currentCrewTitle == "Engineer" && this.type == "Train") || (currentCrewTitle == "Captain" && this.type == "Boat")) {
         console.log (`We have a ${JSON.stringify(currentCrewTitle, null, 2)} and a ${this.type}. Let's GO!`)
         return true
       }
-      
-
-    
-      
-      //if this vehicle is Plane and this crew member's title is Pilor 
-      //OR if this vehicle is Bus and this crew member's title is Drive
-      // OR other two
-      //then go
-      
-
-      //then the vehicle can go
     }
+  //else return false
   console.log(`Nobody on this crew can drive a ${this.type}`)
-
-
-//else return false
   }
 }
 
 
-//Titles:
-//pilot (must be present in plane)
-//captain (must be present in boat)
-//engineer (must be present in train)
-//driver(must be present in driver)
+
 class CrewMember {
-  //name
-  //title (pilot, captain, etc.)
-  //Vehicle (can only be assigned to one crewMember at a time)
   //assignedVehicle - the Vehicle this crew member is assigned to
     //(several CrewMembers can be on a vehicle)
   constructor (name, title, assignedVehicle) {
+    //name
      this.name = name
+     //title (pilot, captain, etc.)
      this.title = title
+     //Vehicle (can only be assigned to one crewMember at a time)
      this.assignedVehicle = null
   }
+
   //Methods:
   assignTo(vehicle) {
+
+    //TO DO! if assigned to vehicle already, then need to remove from that crewAssgned array
+    if(this.assignedVehicle !== null) {
+      //find the vehicle for which they are assignedCrew
+      //and remove them from that vehicle
+    }
     //assignTo(Vehicle) - assigns this crewMember to the vehicle passed in
     vehicle.crewAssigned.push(this) //THIS WORKS BY ITSELF
     // console.log(`AT now the vehicle has a crew: ${JSON.stringify(vehicle, null, 2)}`) //This creates a circular structure when I assign the vehicle to the person
@@ -80,10 +74,7 @@ class CrewMember {
     //and update the Vehicle's list of assigned crew members (crewAssigned)
     this.assignedVehicle = vehicle //THIS WORKS BY ITSELF
     // console.log(`AT the crewMember is now: ${JSON.stringify(this, null, 2)}`)
-    
-    //TO DO! if assigned to vehicle already, then need to remove from that crewAssgned array
   }
-
 }
 
 let nina = new CrewMember("Nina", "Pilot")
@@ -99,22 +90,30 @@ let v2 = new Vehicle ("54325", "Bus")
 let v3 = new Vehicle("Betty", "Boat")
 
 nina.assignTo(v1);
-// console.log(`Nina: ${(JSON.stringify(nina, null, 2))}`)
-console.log(`v1's assigned crew: ${v1.crewAssigned}`)
+console.log(`Length of crewAssigned in Nina's assignedVehicle: ${nina.assignedVehicle.crewAssigned.length}`) //THIS! tells me that (a) Nina has an assigned vehicle and (b) the length of that crew is 1 (as it should be, because she's the only one on it so far)
+
+
 shondra.assignTo(v2);
-// console.log(`Shondra's assigned vehicle: ${shondra.assignedVehicle}`)
+// // console.log(`Shondra's assigned vehicle: ${shondra.assignedVehicle}`)
+
 donna.assignTo(v2);
-// marie.assignTo(v1)
 
-v1.canGo(); //true
-// v2.canGo(); //false
-// v3.canGo(); // false
+marie.assignTo(v1)
+console.log(`Length of crewAssigned in Marie's assignedVehicle: ${marie.assignedVehicle.crewAssigned.length}`)
 
-console.log(`Does v2 have a length: ${v2.crewAssigned.length}`) //true
 
-// console.log(`this is nina's assigned vehicle: ${nina.assignedVehicle.identifier}`) //12341
+v1.canGo(); //true, because Nina is assigned to V1, and she is a pilot
+v2.canGo(); //false, because v2 is a bus, and its assignedCrew are Shondra, an engineer, and Donna, a captain
+v3.canGo(); // false, because v3 has no assigned crew; the console.log comes back weird, because it implies there's a crew on the boat
+
+console.log(`Does v2 have a length: ${v2.crewAssigned.length}`) // 2, because Shondra and Donna are assigned to this vehicle
+
+console.log(`this is nina's assigned vehicle: ${nina.assignedVehicle.identifier}`) //12341, because Nina is assigned to v1, and it's identifer is 12341
 
 //if this is too easy, and only after you are done with the above, 
-//what happens if you assign a crew member to two vehicles?
-//they need to be removed from the first list and assigned to the second one 
+  //what happens if you assign a crew member to two vehicles?
+      //The crewMember's assignedVehicle will always only by one; it's replaced with this.assignedVehicle = vehicle in canGo()
 
+  //they need to be removed from the first list and assigned to the second one 
+
+nina.assignTo(v3)
